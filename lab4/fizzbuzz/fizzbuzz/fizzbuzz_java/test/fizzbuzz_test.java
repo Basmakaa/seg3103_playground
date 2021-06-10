c463a8139641d2dfd0df67318d27519ecfacf8b3
Basma Kaanane <bkaan006@uottawa.ca>
2021-06-10 15:23:30 -0400

    1st test failed

---
+++ lab4/fizzbuzz/fizzbuzz/fizzbuzz_java/test/fizzbuzz_test.java
@@ -0,0 +1,16 @@
+package fizzbuzz_java.test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class fizzbuzz_test {
+    
+	private String fizzBuzz(int num){
+		return "0";
+	}
+
+	@Test
+	public void test_fizzBuzz1(){
+		assertEquals("1", fizzBuzz(1));
+	}
+    
+}
