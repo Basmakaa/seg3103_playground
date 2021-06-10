a116ebbf8b5c882daca8b62051785840058008d6
Basma Kaanane <bkaan006@uottawa.ca>
2021-06-10 13:58:15 -0400

    added lab4 files

---
+++ lab4/tic/tic/lib/tic.ex
@@ -0,0 +1,18 @@
+defmodule Tic do
+  @moduledoc """
+  Documentation for `Tic`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tic.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
