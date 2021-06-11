2a2e0a28c0329886e19187ea2c9f8ab736957d1c
Basma Kaanane <bkaan006@uottawa.ca>
2021-06-10 23:14:22 -0400

    RIGHT answer for Q2.1 AVG method

--- devoir2/grades/lib/grades/calculator.ex
+++ devoir2/grades/lib/grades/calculator.ex
@@ -1,37 +1,19 @@
 defmodule Grades.Calculator do
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    #Q2.1 AVG method
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
     round(mark * 100)
   end
 
   def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
+    #Q2.1 AVG method
+    avg_homework = avg(homework)
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_labs = avg(labs)
+
 
     avg_exams = (midterm + final) / 2
 
@@ -62,19 +44,10 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
+   #Q2.1 AVG method
+    avg_homework = avg(homework)
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_labs = avg(labs)
 
     avg_exams = (midterm + final) / 2
 
@@ -103,4 +76,13 @@ defmodule Grades.Calculator do
       end
     end
   end
+  # Q2.1 AVG method
+  def avg(note) do
+    if Enum.count(note) == 0 do
+          0
+        else
+          Enum.sum(note) / Enum.count(note)
+        end
+    end
+
 end
